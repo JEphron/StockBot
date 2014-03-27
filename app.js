63339c0de330092abeedd1504f4d4ec59e5ccaf8
JEphron <Jephron63@gmail.com>
2014-03-26 22:11:40 -0400

    Added ability to view orders / refactored credentials

--- app.js
+++ app.js
@@ -2,18 +2,23 @@ var http = require('http'),
     async = require('async'),
     API = require('./marketAPI');
 
-var credentials = {
+API.init({
     password: 'immabot',
-    email: 'a405312@drdrb.net'
-};
+    email: 'a405312@drdrb.net',
+    gameName: 'testpleaseignore',
+    gamePassword: 'nodejs'
+});
 
 // Do all the bullshit
 async.series([
 
     function(callback) {
-        API.login(credentials, callback);
+        API.login(callback);
     },
     function(callback) {
-        API.placeOrder('STOCK-XNAS-ZNGA', 1000, 'Buy', callback);
+        API.placeOrder('STOCK-XNAS-ZNGA', 550, 'Buy', callback);
+    },
+    function(callback) {
+        API.loadOrders(callback);
     }
 ]);
--- marketAPI.js
+++ marketAPI.js
@@ -1,9 +1,17 @@
-var request = require('request').defaults({
-    jar: true // enable cookies in request
-});
+var cheerio = require('cheerio'),
+    request = require('request').defaults({
+        jar: true // enable cookies in request
+    });
+
+var credentials = {};
+
+function init(creds) {
+    credentials = creds;
+}
 
 // Login to the server
-function login(creds, callback) {
+function login(callback) {
+    console.log(credentials)
     var opts = {
         url: 'https://id.marketwatch.com/auth/submitlogin.json',
         headers: {
@@ -16,8 +24,8 @@ function login(creds, callback) {
             'Cache-Control': 'no-cache'
         },
         json: {
-            password: creds.password,
-            username: creds.email,
+            password: credentials.password,
+            username: credentials.email,
             realm: 'default',
             savelogin: 'true',
             template: 'default',
@@ -43,12 +51,12 @@ function login(creds, callback) {
 // Submit an order to the server
 function placeOrder(stockID, shares, orderType, callback) {
     var opts = {
-        url: 'http://www.marketwatch.com/game/testpleaseignore/trade/submitorder?week=1',
+        url: 'http://www.marketwatch.com/game/' + credentials.gameName + '/trade/submitorder?week=1', // DANGER DANGER WHAT IS THIS EVEN?
         headers: {
             'Host': 'www.marketwatch.com',
             'Origin': 'http://www.marketwatch.com',
             'Pragma': 'no-cache',
-            'Referer': 'http://www.marketwatch.com/game/testpleaseignore/trade',
+            'Referer': 'http://www.marketwatch.com/game/' + credentials.gameName + '/trade',
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36',
             'X-Requested-With': 'XMLHttpRequest',
             'Connection': 'keep-alive',
@@ -74,5 +82,28 @@ function placeOrder(stockID, shares, orderType, callback) {
     });
 };
 
+// Get the portfolio page and parse the orders table
+// Return a list of orders
+function loadOrders(callback) {
+    request.get('http://www.marketwatch.com/game/' + credentials.gameName + '/portfolio/orders', function(err, res, body) {
+        var $ = cheerio.load(body);
+        var orders = [];
+        // for each table row in the orders table
+        $("section.portfolio > table > tbody tr").each(function(i, el) {
+            // get each cell in the row
+            var tds = $(el).find('td');
+            // hopefully they won't change their format any time soon. 
+            orders[i] = {
+                stockName: $(tds[0]).text().replace(/\t|\n|\r/g, ''), // jeez, these guys have some real messed up html
+                count: $(tds[1]).text().replace(/\t|\n|\r/g, ''),
+                type: $(tds[2]).text().replace(/\t|\n|\r/g, ''),
+                activationDate: $(tds[3]).text().replace(/\t|\n|\r/g, '')
+            };
+        });
+        console.log(orders);
+    });
+}
+module.exports.init = init;
 module.exports.login = login;
 module.exports.placeOrder = placeOrder;
+module.exports.loadOrders = loadOrders;
