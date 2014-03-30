5a8538a94851e34e9ebf3c2e273cd79539cb67f4
JEphron <Jephron63@gmail.com>
2014-03-29 23:19:41 -0400

    added comments to loadPrevious example

--- app.js
+++ app.js
@@ -38,18 +38,16 @@ function onTimestep(data, callback) {
     var action = "none";
     var amount = 50;
     var historicalSnapshots = [];
-    Sync(function() {
-        var prev = data.loadPrevious.sync(null, 1);
-        console.log(data.dataSymbol);
-        console.log(prev[0].dataValues);
-    });
 
-    // for (var i = 0; i < 5; i++) {
-    //     Sync(function() {
-
-    //         historicalSnapshots.push(prev);
-    //     });
-    // }
+    // will attempt to load the last n previous snapshots so we can try to make educated decisions
+    // if we are less than n timesteps from the start of the program, 
+    // the results of queries with indices greater than the current timestep will be null
+    for (var i = 1; i < 5; i++) {
+        Sync(function() {
+            var prev = data.loadPrevious.sync(null, i);
+            historicalSnapshots.push(prev);
+        });
+    }
 
 
 
